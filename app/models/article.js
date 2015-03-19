72e76cb668cf18766677804a59b6f7b446920b2c
Gharbi Mohammed <simo163@hotmail.co.uk>
2015-03-19 11:54:03 +0100

    fetching a friend articles

--- app/models/article.js
+++ app/models/article.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   createdAt: DS.attr('date'),
-  descritpion: DS.attr('string'),
+  description: DS.attr('string'),
   friend: DS.belongsTo('friend'),
   notes: DS.attr('string'),
   state: DS.attr('string')
