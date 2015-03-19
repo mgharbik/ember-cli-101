7c1b673d00dd816b7d775598f49631192c7f1e20
Gharbi Mohammed <simo163@hotmail.co.uk>
2015-03-19 22:55:49 +0100

    consuming momentJs via ember-browserify as if it was other module in the app

--- Brocfile.js
+++ Brocfile.js
@@ -21,7 +21,6 @@ app.import('vendor/fontello/font/fontello.svg', {
 app.import('vendor/fontello/font/fontello.woff', {
 	destDir: 'font'
 });
-app.import('bower_components/moment/moment.js');
 
 // If you need to use different assets in different
 // environments, specify an object as the first parameter. That
