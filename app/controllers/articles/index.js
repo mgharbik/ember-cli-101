2515afe3579ef85624ce8f41e0a7fc8baf0d8601
Gharbi Mohammed <simo163@hotmail.co.uk>
2015-03-20 22:41:47 +0100

    observing collections

--- app/controllers/articles/index.js
+++ app/controllers/articles/index.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	possibleStates: ["borrowed", "returned"]
+	possibleStates: ["borrowed", "returned"],
+
+	contentDidChange: function(){
+		console.log('Called when add or remove an article.');
+	}.observes('model.[]'),
+	
+	StateDidChange: function(){
+		console.log('Called when the state property changed for any of the articles.');		
+	}.observes('model.@each.state')
 });
