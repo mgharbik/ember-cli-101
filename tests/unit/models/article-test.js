17e97d476a109fa836343578b49872714e3af702
Gharbi Mohammed <simo163@hotmail.co.uk>
2015-03-21 11:03:09 +0100

    testing article model

--- tests/unit/models/article-test.js
+++ tests/unit/models/article-test.js
@@ -1,15 +1,24 @@
-import {
-  moduleForModel,
-  test
-} from 'ember-qunit';
+import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
 
-moduleForModel('article', {
+moduleForModel('article', 'Article', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:friend']
 });
 
 test('it exists', function(assert) {
   var model = this.subject();
-  // var store = this.store();
-  assert.ok(!!model);
+  assert.ok(model);
+});
+
+test('state to be borrowed by default', function(assert){
+	var model = this.subject({description: 'Article for test'});
+	assert.equal(model.get('state'), 'borrowed');
+});
+
+test('friend relationship', function(assert){
+	var klass = this.subject({}).constructor;
+	var relationship = Ember.get(klass, 'relationshipsByName').get('friend');
+	assert.equal(relationship.key, 'friend');
+	assert.equal(relationship.kind, 'belongsTo');
 });
