45145e9a94f0540c90bb43f089a8f614cbc6e16e
Gharbi Mohammed <simo163@hotmail.co.uk>
2015-03-23 22:16:19 +0100

    installing i18n plugin

---
+++ app/locales/en.js
@@ -0,0 +1,10 @@
+export default {
+	header: {
+		dashboard: 'Dashboard',
+		friends: 'Friends',
+		new_friend: 'New Friend'
+	},
+	index: {
+		total_friends: 'Total Friends'
+	}
+};
---
+++ app/locales/fr.js
@@ -0,0 +1,10 @@
+export default {
+	header: {
+		dashboard: 'Dashboard',
+		friends: 'Amis',
+		new_friend: 'Nouveau Ami'
+	},
+	index: {
+		total_friends: 'Total Amis'
+	}
+};
